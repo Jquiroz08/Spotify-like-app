42dbaa07da3ceba186ce6efc048ad02a2d1b7d28
Jquiroz08 <joqu6657@stu.sjcc.edu>
2024-11-01 19:58:24 -0700

    Code spacing cleanup

--- src/demo/src/main/java/com/example/SpotifyLikeAppExampleCode.java
+++ src/demo/src/main/java/com/example/SpotifyLikeAppExampleCode.java
@@ -13,32 +13,40 @@ public class SpotifyLikeAppExampleCode {
   private static Clip audioClip;
 
   /*
-    *** IMPORTANT NOTE FOR ALL STUDENTS ***
+   *** IMPORTANT NOTE FOR ALL STUDENTS ***
+   * 
+   * This next line of code is a "path" that students will need to change in order
+   * to play music on their
+   * computer. The current path is for my laptop, not yours.
+   * 
+   * If students who do not understand whre files are located on their computer or
+   * how paths work on their computer,
+   * should immediately complete the extra credit on "Folders and Directories" in
+   * the canvas modules.
+   * 
+   * Knowing how paths work is fundamental knowledge for using a computer as a
+   * technical person.
+   * 
+   * The play function in this example code plays one song. Once student change
+   * the directoryPath variable properly,
+   * one of the songs should play. Students should implement their own code for
+   * all the functionality in the assignment.
+   * 
+   * Students can use, and find their own music. You do not have to use or listen
+   * to my example music! Have fun!
+   * 
+   * Students who do not know what a path is on their computers, and how to use a
+   * path, are often unable complete
+   * this assignment succesfullly. If this is your situation, please complete the
+   * extra credit on Folders Path,
+   * and Directories. Also, please do google and watch youtube videos if that is
+   * helpful too.
+   * 
+   * Thank you! -Gabriel
+   * 
+   */
 
-    This next line of code is a "path" that students will need to change in order to play music on their
-    computer.  The current path is for my laptop, not yours.
-    
-    If students who do not understand whre files are located on their computer or how paths work on their computer, 
-    should immediately complete the extra credit on "Folders and Directories" in the canvas modules.  
-    
-    Knowing how paths work is fundamental knowledge for using a computer as a technical person.
-
-    The play function in this example code plays one song. Once student change the directoryPath variable properly,
-    one of the songs should play. Students should implement their own code for 
-    all the functionality in the assignment.
-    
-    Students can use, and find their own music. You do not have to use or listen to my example music! Have fun!
-
-    Students who do not know what a path is on their computers, and how to use a path, are often unable complete
-    this assignment succesfullly. If this is your situation, please complete the extra credit on Folders Path,
-    and Directories. Also, please do google and watch youtube videos if that is helpful too.
-    
-    Thank you! -Gabriel
-
-    */
-
-  private static String directoryPath =
-    "C:\\Users\\josep\\OneDrive\\Desktop\\Git Repo\\Spotify-like-app\\src\\demo\\src\\main\\java\\com\\example\\";
+  private static String directoryPath = "C:\\Users\\josep\\OneDrive\\Desktop\\Git Repo\\Spotify-like-app\\src\\demo\\src\\main\\java\\com\\example\\";
 
   // String Array to display "Recently Played" in Home menu
   private static ArrayList<String> recentlyPlayed = new ArrayList<String>();
@@ -50,7 +58,6 @@ public class SpotifyLikeAppExampleCode {
   public static void main(final String[] args) {
     // reading audio library from json file
     Song[] library = readAudioLibrary();
-
 
     String userInput = "";
     while (!userInput.equals("q")) {
@@ -84,35 +91,35 @@ public class SpotifyLikeAppExampleCode {
   }
 
   // Prints out a list of recently played songs
-  public static void home(){
+  public static void home() {
     System.out.println("Recently Played: ");
-    for(int i=0; i<recentlyPlayed.size(); i++){
-      System.out.println(i+1 +": " + recentlyPlayed.get(i));
+    for (int i = 0; i < recentlyPlayed.size(); i++) {
+      System.out.println(i + 1 + ": " + recentlyPlayed.get(i));
     }
   }
-  
+
   // Searches for song name inputed through Song array and plays it if found
-  public static void search(Song[] library){
-   System.out.println("Please enter song title");
-   Scanner input = new Scanner(System.in);
-   String title = input.nextLine();
-   for(int i=0; i<library.length; i++){
-      if(title.equalsIgnoreCase(library[i].name())){
+  public static void search(Song[] library) {
+    System.out.println("Please enter song title");
+    Scanner input = new Scanner(System.in);
+    String title = input.nextLine();
+    for (int i = 0; i < library.length; i++) {
+      if (title.equalsIgnoreCase(library[i].name())) {
         play(library, i);
         break;
       }
-   }
+    }
   }
 
   // Prints library of songs and plays the user-selected song
   public static void library(Song[] library) {
-    for(int i=0; i<library.length; i++){
-       System.out.println(i+1 + ": "+ library[i].name() + " by " + library[i].artist());
+    for (int i = 0; i < library.length; i++) {
+      System.out.println(i + 1 + ": " + library[i].name() + " by " + library[i].artist());
     }
     Scanner input = new Scanner(System.in);
     System.out.println("Please select which song to play");
     int index = input.nextInt();
-    play(library, index-1);
+    play(library, index - 1);
   }
 
   /*
@@ -170,9 +177,11 @@ public class SpotifyLikeAppExampleCode {
     } catch (Exception e) {
       e.printStackTrace();
     }
+    
     // Adds song to Recently played list
     recentlyPlayed.add(library[i].name() + " by " + library[i].artist());
-    //Prints information of song when it starts playing
+
+    // Prints information of song when it starts playing
     System.out.println("---------------------");
     System.out.println("Currently Playing: ");
     System.out.println("Song name: " + library[i].name());
